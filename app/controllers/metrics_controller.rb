9f82374c717cefc2a862c0750b7d749b4d44f73c
foster154 <foster154@gmail.com>
2014-09-08 08:17:42 -0600

    monthly metrics

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -24,6 +24,55 @@ class MetricsController < ApplicationController
 	end
 
 	def monthly
+		@date = params[:date] || Date.today
+
+		# current week
+		@week5day1 = @date.to_date.beginning_of_week
+		@week4day1 = @week5day1.advance(days: -7)
+		@week3day1 = @week5day1.advance(days: -14)
+		@week2day1 = @week5day1.advance(days: -21)
+		@week1day1 = @week5day1.advance(days: -28)
+
+		@week1 = [ 	@week1day1, 
+					@week1day1.advance(days: 1), 
+					@week1day1.advance(days: 2), 
+					@week1day1.advance(days: 3), 
+					@week1day1.advance(days: 4), 
+					@week1day1.advance(days: 5), 
+					@week1day1.advance(days: 6) ]
+
+		@week2 = [ 	@week2day1, 
+					@week2day1.advance(days: 1), 
+					@week2day1.advance(days: 2), 
+					@week2day1.advance(days: 3), 
+					@week2day1.advance(days: 4), 
+					@week2day1.advance(days: 5), 
+					@week2day1.advance(days: 6) ]
+
+		@week3 = [ 	@week3day1, 
+					@week3day1.advance(days: 1), 
+					@week3day1.advance(days: 2), 
+					@week3day1.advance(days: 3), 
+					@week3day1.advance(days: 4), 
+					@week3day1.advance(days: 5), 
+					@week3day1.advance(days: 6) ]
+
+		@week4 = [ 	@week4day1, 
+					@week4day1.advance(days: 1), 
+					@week4day1.advance(days: 2), 
+					@week4day1.advance(days: 3), 
+					@week4day1.advance(days: 4), 
+					@week4day1.advance(days: 5), 
+					@week4day1.advance(days: 6) ]
+
+		@week5 = [ 	@week5day1, 
+					@week5day1.advance(days: 1), 
+					@week5day1.advance(days: 2), 
+					@week5day1.advance(days: 3), 
+					@week5day1.advance(days: 4), 
+					@week5day1.advance(days: 5), 
+					@week5day1.advance(days: 6) ]
+
 	end
 
 end
