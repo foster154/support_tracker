c6632e4119ae3c53b9abcbbdf208d303856a55b3
foster154 <foster154@gmail.com>
2015-12-02 08:55:07 -0700

    small amount of cleanup

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,31 +1,24 @@
 $(document).ready(function() {
 
-	//
-	// PRICING CALCULATOR
-	//
+	// Pricing Calculator
+	// ===========================
 
 	$('#edition').on('click', '.pricing-btn', function() {
 		$(this).addClass('btn-clicked');
 		$(this).siblings().removeClass('btn-clicked');
 		selected_edition = +$(this).data('value');
-		console.log(selected_edition);
 	});
 
 	$('#licenses').on('click', '.pricing-btn', function() {
 		$(this).addClass('btn-clicked');
 		$(this).siblings().removeClass('btn-clicked');
 		selected_licenses = +$(this).data('value');
-		console.log(selected_licenses);
 	});
 
 	$('#pricing').on('click', '#calculate', function(event) {
-		console.log("click detected")
 		event.preventDefault();
-		console.log("default prevented")
 		var per_month = selected_edition * selected_licenses;
-		console.log("'per month' set")
 		var per_year = selected_edition * .9 * selected_licenses * 12;
-		console.log("'per year' set")
 		$('#results').text("$" + per_month + " per month, or $" + per_year + " per year (10% off)" );
 	});
 	
